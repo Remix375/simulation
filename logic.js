7f0fc4cdc3daca81035165bc00553f04ca50d329
Remix375 <bigbossremi@gmail.com>
2023-05-02 12:18:54 +0200

    collision response

--- logic.js
+++ logic.js
@@ -15,6 +15,8 @@ const friction = 0.1;
 const controled = new Circle(20, 0, 0, 0, 0, 0, 0, 0, "green")
 particles.push(controled);
 
+const b = new Circle(40, 100, 100, 0, 0, 0, 0, 10, 'blue')
+particles.push(b);
 
 //boolean variables - if true, ball moves in the direction
 let LEFT, UP, RIGHT, DOWN;
@@ -50,7 +52,6 @@ document.addEventListener('keyup', function(e){
 });
 
 function act(b){
-    console.log(LEFT)
     //if true, the accelertion component gets a certain value
     if(LEFT){
         b.acc.x = -1;
@@ -85,7 +86,7 @@ canvas.addEventListener("click", function(e) {
     const localX = e.clientX - e.target.offsetLeft;
     const localY = e.clientY - e.target.offsetTop;
 
-    particles.push(new Circle(20, localX / current_zoom, localY / current_zoom, 60, 0, 0, 10, 20, "red"));
+    particles.push(new Circle(20, localX / current_zoom, localY / current_zoom, 0, 0, 0, 0, 20, "red"));
 })
 
 
@@ -101,9 +102,22 @@ function clearDead(l) {
 
 
 
+function ballsTouching(b1, b2) {
+    return (b1.size + b2.size >= b1.pos.subtr(b2.pos).mag())
+}
+
+function pen_res_bb(b1, b2){
+    let dist = b1.pos.subtr(b2.pos);
+    let pen_depth = b1.size + b2.size - dist.mag();
+    let pen_res = dist.unit().mult(pen_depth/2);
+    b1.pos = b1.pos.add(pen_res);
+    b2.pos = b2.pos.add(pen_res.mult(-1));
+}
+
 const mainLoop = () => {
     ctx.canvas.width = window.innerWidth * 0.6;
     ctx.canvas.height = window.innerHeight * 0.7;
+
 
 
     timeCount += 1
@@ -118,8 +132,15 @@ const mainLoop = () => {
 
     drawScale(ctx, current_zoom);
     for (let elt = 0; elt < particles.length; elt++) {
+        for (let elt2 = elt+1; elt2 < particles.length; elt2++) {
+            console.log(particles[elt].size)
+            if (ballsTouching(particles[elt], particles[elt2])){
+                pen_res_bb(particles[elt], particles[elt2]);
+            }
+        }
         particles[elt].draw(ctx, current_zoom);
         particles[elt].update(friction);
+        
     }
     requestAnimationFrame(mainLoop);
 }
--- particle.js
+++ particle.js
@@ -1,33 +1,50 @@
 
 
-class Vector {
-    constructor(x, y) {
+class Vector{
+    constructor(x, y){
         this.x = x;
         this.y = y;
     }
 
-    add(v) {
-        this.x += v.x;
-        this.y += v.y;
+    add(v){
+        return new Vector(this.x+v.x, this.y+v.y);
     }
 
-    subtract(v) {
-        this.x -= v.x;
-        this.y -= v.y;
+    subtr(v){
+        return new Vector(this.x-v.x, this.y-v.y);
     }
 
-    mult(n) {
-        this.x *= n;
-        this.y *= n;
+    mag(){
+        return Math.sqrt(this.x**2 + this.y**2);
     }
 
-    magn() {
-        return Math.sqrt(this.x ** 2 + this.y**2);
+    mult(n){
+        return new Vector(this.x*n, this.y*n);
     }
 
+    normal(){
+        return new Vector(-this.y, this.x).unit();
+    }
 
-    static dot(v1, v2) {
-        return v1.x * v2.x + v1.y * v2.y;
+    unit(){
+        if(this.mag() === 0){
+            return new Vector(0,0);
+        } else {
+            return new Vector(this.x/this.mag(), this.y/this.mag());
+        }
+    }
+
+    drawVec(start_x, start_y, n, color){
+        ctx.beginPath();
+        ctx.moveTo(start_x, start_y);
+        ctx.lineTo(start_x + this.x * n, start_y + this.y * n);
+        ctx.strokeStyle = color;
+        ctx.stroke();
+        ctx.closePath();
+    }
+    
+    static dot(v1, v2){
+        return v1.x*v2.x + v1.y*v2.y;
     }
 }
 
@@ -38,9 +55,15 @@ class Circle {
 
         this.pos = new Vector(posX, posY);
 
-        this.speed = new Vector(speedX/60, speedY/60);
+        //speed given in meters per second
+        //converted to pixels per 1/60 seconds
+        //100 pixels is a meter
+        this.speed = new Vector(100*speedX/60, 100*speedY/60);
 
-        this.acc = new Vector(accX/60, accY/60);
+        //acc given in meters per second^2
+        //converted to pixels per 1/60 seconds ^2
+        //100 pixels is a meter
+        this.acc = new Vector(100*accX/3600, 100*accY/3600);
 
         this.mass = mass
 
@@ -55,6 +78,11 @@ class Circle {
         return this.dead
     }
 
+    pr() {
+        return [this.size, this.pos.x, this.pos.y, this.speed.x, this.speed.y, this.acc.x, this.acc.y, this.mass, this.color]
+    }
+
+
     draw(ctx, zoom) {
         ctx.beginPath();
         ctx.fillStyle = this.color;
@@ -65,10 +93,9 @@ class Circle {
     
 
     update(friction) {
-        this.pos.add(this.speed);
+        this.pos = this.pos.add(this.speed);
 
-        this.speed.add(this.acc);
-        this.speed.mult(1-friction);
+        this.speed = this.speed.add(this.acc).mult(1-friction);
 
         if (this.pos.y > canvas.clientHeight/0.1 + 50 || this.pos.y < -40 || this.pos.x > canvas.clientWidth/0.1 + 50 || this.pos.x < -40) {
             this.dead = true;
